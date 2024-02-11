8ec7678eecc0a6f5428921de823e15543bbcc93f
shleecloud <shleecloud@gmail.com>
2024-02-12 02:10:11 +0900

    init: react-router-dom

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,9 +1,7 @@
-function App() {
+export default function App() {
   return (
     <>
       <h1 className="font-bold text-red-500">Vite + React 18 + Tailwind</h1>
     </>
   );
 }
-
-export default App;
