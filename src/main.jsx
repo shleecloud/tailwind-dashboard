a3fc42320a9d65d1819c4934b74ffefdb6b3a364
shleecloud <shleecloud@gmail.com>
2024-02-12 09:59:08 +0900

    refactor: router

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import { Router } from "./Router.jsx";
-import { RouterProvider } from "react-router-dom";
+import "./index.css"; // for tailwindcss
+import {
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  Outlet,
+} from "react-router-dom";
+import App from "./pages/App";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route
+      element={
+        <div className="h-dvh w-dvw">
+          <Outlet />
+        </div>
+      }
+    >
+      <Route element={<App />} path="/" />
+    </Route>,
+  ),
+);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={Router} />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
