fcfa91aceac4370539466e2f8a59b3943e8d0e4e
shleecloud <shleecloud@gmail.com>
2024-02-18 18:56:31 +0900

    feature: create data picker with range

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,16 @@
+import { Button } from "@/components/ui/button";
+import { DatePickerWithRange } from "@/components/date-range-picker";
+
 export default function Home() {
-  return <section>Home</section>;
+  return (
+    <section>
+      <div className="flex justify-between items-center my-4">
+        <h1 className="text-3xl font-bold tracking-tight">Dashboard</h1>
+        <div className="flex gap-1">
+          <DatePickerWithRange />
+          <Button className="">Download</Button>
+        </div>
+      </div>
+    </section>
+  );
 }
